ceca70d509158e795db00f2130bd353513f3792e
Pandaradox <jacardarella@gmail.com>
2018-09-04 13:28:21 -0500

    Init Commit

---
+++ initials.py
@@ -0,0 +1,10 @@
+##################################################
+
+
+def main():
+    for i in range(1, int(input("How many dishes?"))+1):
+        print("{}. Ham and Eggs".format(i))
+
+
+if __name__ == "__main__":
+    main()
