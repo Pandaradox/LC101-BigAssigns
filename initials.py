22a0cae3c829e478b0645962042c702556442865
Pandaradox <jacardarella@gmail.com>
2018-09-07 10:29:53 -0500

    Final week commit

--- initials.py
+++ initials.py
@@ -1,9 +1,20 @@
 ##################################################
 
 
+def get_initials(fullname):
+    """ Given a person's name, returns the person's initials (uppercase) """
+    # TODO your code here
+    fullname = (fullname.upper()).strip()
+    initials = [fullname[0]]
+    # Scan for spaces and grab the following letter
+    for letter in range(len(fullname)-1):
+        if fullname[letter] == " ":
+            initials.append(fullname[letter+1])
+    return("".join(initials))
+
+
 def main():
-    for i in range(1, int(input("How many dishes?"))+1):
-        print("{}. Ham and Eggs".format(i))
+    print(get_initials(input("What's your full name?")))
 
 
 if __name__ == "__main__":
