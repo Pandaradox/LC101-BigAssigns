7192bce1513365881e97254cbaae9bd6f4cd7e12
Pandaradox <jacardarella@gmail.com>
2018-09-18 13:39:58 -0500

    Tueday Commit

---
+++ crypto.py
@@ -0,0 +1,12 @@
+# ########################################Crypto Big Assign
+#
+#
+# def main():
+#
+#     print("Ham and Eggs")
+#
+#
+# if __name__ == "__main__":
+#     main()
+dict = {1: "Hello", 2: "World"}
+print(list(dict))
